774ed91d10ebc27e08254946f8ad1cca509224a2
David Souto <csouto.david@gmail.com>
2025-07-17 13:49:54 -0300

    atualizando arquivo de login que não subiu com ultimas alterações

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,6 @@ describe('Login', () => {
     cy.visit('http://localhost:4000')
 
     cy.get('#username').click().type('julio.lima')
-
     cy.get('#senha').click().type('123456')
     cy.contains('Entrar').click()
 
