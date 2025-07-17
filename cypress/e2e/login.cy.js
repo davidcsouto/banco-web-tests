24fd7f5ad3fb3b4147872457886edc915d811149
David Souto <csouto.david@gmail.com>
2025-07-17 14:47:16 -0300

    Adicionando screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,14 @@
 describe('Login', () => {
   beforeEach(()=> {
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-pagina')
   })
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  it.only('Login com dados válidos deve permitir entrada no sistema', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('apos-preencher-dados-válidos')
     cy.contains('Entrar').click()
+    cy.screenshot('apos-clicar-no-botao-entrar')
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
