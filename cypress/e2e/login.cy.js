3770598612b599ad62f1edbe1f31814b5a3cf1ce
David Souto <csouto.david@gmail.com>
2025-07-22 15:07:10 -0300

    alterando a origem da URL

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login', () => {
   beforeEach(()=> {
-    cy.visit(Cypress.env('URL'))
+    cy.visit('/')
     cy.screenshot('apos-visitar-pagina')
   })
   it('Login com dados válidos deve permitir entrada no sistema', () => {
