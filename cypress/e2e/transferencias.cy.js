0d9b54b199ed51a44e874acc185a908d9620a5f7
David Souto <csouto.david@gmail.com>
2025-07-22 16:24:54 -0300

    Implementando comandos customizados para Login, Transferências e Commons

--- cypress/e2e/transferencias.cy.js
+++ cypress/e2e/transferencias.cy.js
@@ -1,24 +1,18 @@
 describe('Transferências', ()=> {
     beforeEach(() => {
         cy.visit('/')
-        cy.fixture('credenciais').then(credenciais => {
-            cy.get('#username').click().type(credenciais.valida.usuario)
-            cy.get('#senha').click().type(credenciais.valida.senha)
-        })
-        cy.contains('Entrar').click()
+        cy.fazerLoginComCredenciaisValidas()
     })
+
     it('Deve transferir quando informo dados e valor válidos', () => {
-        cy.get('label[for="conta-origem"]').parent().as('campo-conta-origem')
-        cy.get('@campo-conta-origem').click()
-        cy.get('@campo-conta-origem').contains('Maria Oliveira').click()
+        cy.realizarTransferencia('Maria Oliveira', 'João da Silva', 11)
 
-        cy.get('label[for="conta-destino"]').parent().as('campo-conta-destino')
-        cy.get('@campo-conta-destino').click()
-        cy.get('@campo-conta-destino').contains('João da Silva').click()
+        cy.verificarMensagemNoToast('Transferência realizada!')
+    })
 
-        cy.get('#valor').type('11')
-        cy.contains('Transferir').click()
+    it('Deve apresentar erro quando tentar mais que 5 mil sem o token', () => {
+        cy.realizarTransferencia('Maria Oliveira', 'João da Silva', 5000.01)
 
-        cy.get('.toast').should('be.visible').and('have.text', 'Transferência realizada!')
+        cy.verificarMensagemNoToast('Autenticação necessária para transferências acima de R$5.000,00.')
     })
 })
